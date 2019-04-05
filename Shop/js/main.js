37681781598ddec8d3c6e317a382de068bd1bc17
ToSPP <esau@yandex.ru>
2019-04-05 12:00:44 +0300

    Изменены реализации классов Cart и CartItem

--- Shop/js/main.js
+++ Shop/js/main.js
@@ -11,11 +11,11 @@ class ProductsList {
 
     fetchProducts() {
         this.products = [
-            {title: 'Notebook', price: 2000, img: 'https://placehold.it/200x150'},
-            {title: 'Mouse', price: 20, img: 'https://placehold.it/200x150'},
-            {title: 'Keyboard', price: 48, img: 'https://placehold.it/200x150'},
-            {title: 'Gamepad', price: 63, img: 'https://placehold.it/200x150'},
-            {title: 'Chair', price: 200, img: 'https://placehold.it/200x150'},
+            {title: 'Notebook', price: 2000},
+            {title: 'Mouse', price: 20},
+            {title: 'Keyboard', price: 48},
+            {title: 'Gamepad', price: 63},
+            {title: 'Chair', price: 200},
         ];
     }
 
@@ -23,10 +23,8 @@ class ProductsList {
      * Метод считает общую стоимость товаров каталога
      * @returns {int} Стоимость товаров
      */
-    countProductsTotalCost() {
-        return this.products.reduce((acc, cur) => {
-            return acc + cur.price;
-        }, 0);
+    countProductsTotalPrice() {
+        return this.products.reduce((acc, cur) => acc + cur.price, 0);
     }
 
     render() {
@@ -39,49 +37,81 @@ class ProductsList {
 }
 
 class Product {
-    constructor(product) {
+    constructor(product, img = 'https://placehold.it/200x150') {
         this.title = product.title;
         this.price = product.price;
-        this.img = product.img;
-        this._classes();
-    }
-    _classes() {
-        this.classes = {
-            itemClass: 'product-item',
-            descClass: 'product-item__desc',
-            btnClass: 'product-item__btn-buy',
-            btnTitle: 'Купить',
-        };
+        this.img = img;
     }
 
     render() {
-        return `<div class="${this.classes.itemClass}">
+        return `<div class="product-item">
                     <img src="${this.img}" alt="Some img">
-                    <div class="${this.classes.descClass}">
+                    <div class="product-item__desc">
                         <h3>${this.title}</h3>
-                        <p>${this.price} $</p>
-                        <button class="${this.classes.btnClass}">${this.classes.btnTitle}</button>
+                        <p>${this.price} руб.</p>
+                        <button class="product-item__btn-buy">Купить</button>
                     </div>
                 </div>`;
     }
 }
 
-class CartItem extends Product {
-    _classes() {
-        this.classes = {
-            itemClass: 'cart-item',
-            descClass: 'cart-item__desc',
-            btnClass: 'cart-item__btn btn-cancel',
-            btnTitle: '&#10006;',
-        };
+class CartItem {
+    constructor(product, img = 'https://placehold.it/75x75') {
+        this.title = product.title;
+        this.price = product.price;
+        this.img = img;
+    }
+
+    render() {
+        return `<div class="cart-item">
+                    <img src="${this.img}" alt="Some img">
+                    <div class="cart-item__desc">
+                        <h3>${this.title}</h3>
+                        <p>${this.price} руб.</p>
+                        <button class="cart-item__btn btn-cancel">&#10006;</button>
+                    </div>
+                </div>`;
     }
 }
 
-class Cart extends ProductsList {
+class Cart {
+    constructor() {
+        this.products = [];
+        this.init();
+    }
+
+    init() {
+        this.fetchProducts();
+        this.render();
+    }
+
+    static toggleCart() {
+        document.querySelector('.cart').classList.toggle('cart_active');
+    }
+
+    static hideCart() {
+        document.querySelector('.cart').classList.remove('cart_active');
+    }
+
+    addProduct(product) {
+        // если уже добавлен изменить количество и общую стоимость
+        // если нет - отрисовать
+    }
+
+    removeProduct(product) {
+        // если в корзине больше, чем один продукт - изменить количество и общую ст-ть
+        // если 1 продукт - удалить элемент
+    }
+
+    countTotalAmount() {
+        // считает общую сумму корзины
+        return this.products.reduce((acc, cur) => acc + cur.price, 0);
+    }
+
     fetchProducts() {
         this.products = [
-            {title: 'Notebook', price: 2000, img: 'https://placehold.it/75x75'},
-            {title: 'Chair', price: 200, img: 'https://placehold.it/75x75'},
+            {title: 'Notebook', price: 2000},
+            {title: 'Chair', price: 200},
         ];
     }
 
