7b1d5b877451f5d06fcfdaa9bc2dd974c0ad3c1c
ToSPP <esau@yandex.ru>
2019-04-06 01:22:29 +0300

    Реализованы методы добавления продукта в корзину и удаления продукта из корзины + мелкие правки

--- Shop/js/main.js
+++ Shop/js/main.js
@@ -59,7 +59,8 @@ class Product {
                     <div class="product-item__desc">
                         <h3>${this.product_name}</h3>
                         <p>${this.price} руб.</p>
-                        <button class="product-item__btn-buy" data-id="${this.id_product}">Купить</button>
+                        <button class="product-item__btn-buy" data-id="${this.id_product}" 
+                                data-name="${this.product_name}" data-price="${this.price}">Купить</button>
                     </div>
                 </div>`;
   }
@@ -75,7 +76,7 @@ class CartItem {
   }
 
   render() {
-    return `<div class="cart-item">
+    return `<div class="cart-item" data-id="${this.id_product}">
                     <img src="${this.img}" alt="Some img">
                     <div class="cart-item__desc">
                         <h3>${this.product_name}</h3>
@@ -134,45 +135,108 @@ class Cart {
     }
   }
 
-  addProduct(id) {
-    // если уже добавлен изменить количество и общую стоимость
-    // если нет - отрисовать
-    console.log(id);
-    console.log(this.contents);
+  addProduct(product) {
+    // Найдем индекс продукта, если он уже присутствует в корзине
+    const idx = this.contents.findIndex(el => el.id_product === product.id_product);
+
+    if (!!~idx) {
+      // Если такой продукт уже есть в корзине
+
+      // Увеличиваем количество
+      const qty = ++this.contents[idx].quantity;
+      // Увеличиваем общую сумму корзины
+      this.amount += this.contents[idx].price;
+      // Находим нужный элемент
+      const element = document.querySelector(`.cart__items [data-id='${product.id_product}']`);
+      // Присвоим новое количество
+      element.querySelector('.cart-item__qty').textContent = qty;
+      // Присвоим новую сумму
+      element.querySelector('.cart-item__sum').textContent = qty * this.contents[idx].price;
+    } else {
+      // Иначе - создаем новый элемент корзины и добавляем его
+      const prod = new CartItem(product);
+      this.contents.push(prod);
+      this.amount += prod.price;
+      // Если корзина была пуста, то добавим "Итого"
+      if (0 >= this.countGoods++) {
+        this._renderCartTotal();
+      }
+      // Добавим элемент в DOM
+      document.querySelector('.cart__items').insertAdjacentHTML('beforeend', prod.render());
+    }
+    // Отобразим общую сумму
+    this.setTotalAmount();
   }
 
-  removeProduct(id) {
-    // если в корзине больше, чем один продукт - изменить количество и общую ст-ть
-    // если 1 продукт - удалить элемент
-  }
+  removeProduct(product) {
+    // Найдем индекс продукта в массиве
+    const idx = this.contents.findIndex(el => el.id_product === product.id_product);
 
-  _countTotalAmount() {
-    return this.contents.reduce((acc, cur) => acc + cur.price, 0);
+    if (!!~idx) {
+      // Находим нужный элемент
+      const element = document.querySelector(`.cart__items [data-id='${product.id_product}']`);
+
+      if (this.contents[idx].quantity > 1) {
+        // Если в корзине количество продукта больше, чем 1
+
+        // Уменьшаем количество
+        const qty = --this.contents[idx].quantity;
+        // Уменьшаем общую сумму корзины
+        this.amount -= this.contents[idx].price;
+        // Присвоим новое количество
+        element.querySelector('.cart-item__qty').textContent = qty;
+        // Присвоим новую сумму
+        element.querySelector('.cart-item__sum').textContent = qty * this.contents[idx].price;
+      } else {
+        // Иначе - уменьшаем количество продуктов в корзине
+        this.countGoods--;
+        // Уменьшаем общую сумму корзины
+        this.amount -= this.contents[idx].price;
+        // Удаляем элемент из DOM
+        element.remove();
+        // Удаляем продукт из общего массива
+        this.contents.splice(idx, 1);
+      }
+    }
+
+    if (0 >= this.countGoods) {
+      // Если в корзине нет продуктов - удалим общую сумму
+      document.querySelector('.cart-total').remove();
+    } else {
+      // Отобразим общую сумму
+      this.setTotalAmount();
+    }
   }
 
   setTotalAmount() {
     if (this.countGoods > 0) {
-      document.querySelector('.cart-total_value').textContent = this._countTotalAmount();
+      document.querySelector('.cart-total_value').textContent = this.amount;
     }
   }
 
   render() {
-    const block = document.querySelector('.cart');
+    const cart = document.querySelector('.cart');
+    const cartItemsElem = document.querySelector('.cart__items');
 
     this.contents.forEach(product => {
       const prod = new CartItem(product);
-      block.insertAdjacentHTML('beforeend', prod.render());
+      cartItemsElem.insertAdjacentHTML('beforeend', prod.render());
     });
 
+    cart.insertAdjacentHTML('beforeend', '<button class="btn-goToCart">Перейти в корзину</button>');
+    this._renderCartTotal();
+  }
+
+  _renderCartTotal() {
+    const btn = document.querySelector('.btn-goToCart');
     if (this.countGoods > 0) {
       const totalElem
         = `<div class="cart-total">
            Итого: <span class="cart-total_value">${this.amount}</span> руб.
          </div>`;
-      block.insertAdjacentHTML('beforeend', totalElem);
+      // Вставляем перед кнопкой "Перейти в корзину"
+      btn.insertAdjacentHTML('beforebegin', totalElem);
     }
-
-    block.insertAdjacentHTML('beforeend', '<button class="btn-goToCart">Перейти в корзину</button>');
   }
 }
 
